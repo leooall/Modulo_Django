38fe88dabbaee90dbe50577475cb929370f55b7a
Leonardo Llaupe <leonardollaupe@gmail.com>
2024-05-23 21:20:27 -0400

    primera carga

---
+++ proyecto_de_mi_entorno/proyecto_de_mi_entorno/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for proyecto_de_mi_entorno project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto_de_mi_entorno.settings")
+
+application = get_asgi_application()
